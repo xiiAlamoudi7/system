0a3dc4968e60b1867a22fae5c8b0d344057e2454
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-02-03 22:08:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -652,22 +652,6 @@ client.on('message', msg => {
 };
 });
 
-
-
-client.on('message', message => {
-	var prefix = "-";
-if (message.content.startsWith(prefix + 'tag')) {
-    let args = message.content.split(" ").slice(1);
-if(!args[0]) return message.reply('Write Some Things');  
-
-    figlet(args.join(" "), (err, data) => {
-              message.channel.send("```" + data + "```")
-           })
-}
-});
-
-
-
  client.on('message', message => {
 	    var prefix = "-";
               if(!message.channel.guild) return;
