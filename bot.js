a3b4038807694ac43aa7dcf72b9bc6fa29f9f839
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-02-04 20:38:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1220,49 +1220,6 @@ client.on('message',  message => {
     message.delete()
     message.reply(" احترم نفسك , يمنج الشتايم تماما  ").then(msg => {msg.delete(5000)});;
   };
-	
-client.on('message' , async message => {
-            if(message.content.startsWith(prefix + "ads")) {
-     await message.channel.send("`ارسال الرساله .`").then(e => {
-    let filter = m => m.author.id === message.author.id
-    let tests = '';
-    let time = '';
-    let channel = '';
-    let chaTests = message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
-    .then(collected => {
-      tests = collected.first().content
-      collected.first().delete()
-e.edit("`تكرار الرساله كل ....... دقائق`")
-let chaTime = message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
-.then(co => {
-if(isNaN(co.first().content)) return message.reply("`الوقت بالدقائق ! ارقام فقطٍ`");
-if(co.first().content > 1500 || co.first().content < 1) return message.channel.send("`لا اقل من دقيقه ولا اكثر من يوم`")
-  time = co.first().content
-co.first().delete()
-  e.edit("`ادخل اسم الروم`")
-  let chaChannel = message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
-.then(col => {
-  channel = col.first().content
-col.first().delete()
-  e.edit("`جاري اعداد المعلومات الرجاء الانتظاار...`").then(b => {
-              setTimeout(() => {
-    b.edit(`** تم اعداد المعلومات بنجاح .**`)
-        },2000);
-  })
-  var room = message.guild.channels.find('name' , channel)
-  if(!room) return;
-  if (room) {
-setInterval(() => {
-room.send(tests);
-}, time*60000)
-  }
-})
-})
-})
-        
-})
-}
-});
-	
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
