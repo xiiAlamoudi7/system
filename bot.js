e3322ffcb43925fe7b75b959e8ade5e6fea420c9
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-02-04 22:09:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1190,33 +1190,5 @@ message.channel.send(`${user} has ${inviteCount} invites.`);
 });
   }
 });
- client.on("message", async message => {
-            if(!message.channel.guild) return;
-             if(message.content.startsWith(prefix + 'invite-codes')) {
-let guild = message.guild
-var codes = [""]
-message.channel.send(":postbox: **لقد قمت بأرسال جميع روابط الدعوات التي قمت بأنشائها في الخاص**")
-guild.fetchInvites()
-.then(invites => {
-invites.forEach(invite => {
-if (invite.inviter === message.author) {
-codes.push(`discord.gg/${invite.code}`)
-}
-})
-}).then(m => {
-if (codes.length < 0) {
-    var embed = new Discord.RichEmbed()
-.setColor("#000000")
-.addField(`Your invite codes in ${message.guild.name}`, `You currently don't have any active invites! Please create an invite and start inviting, then you will be able to see your codes here!`)
-message.author.send({ embed: embed });
-return;
-} else {
-    var embed = new Discord.RichEmbed()
-.setColor("#000000")
-.addField(`Your invite codes in ${message.guild.name}`, `Invite Codes:\n${codes.join("\n")}`)
-message.author.send({ embed: embed });
-return;
-}
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
