170395e7a563702b284a7a1f3db1e33ce9fdd37e
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-02-05 18:40:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -813,7 +813,7 @@ let args = message.content.split(' ');
 client.on('guildCreate', guild => {
     var embed = new Discord.RichEmbed()
     .setColor(0x5500ff)
-    .setDescription(`**شكراً لك لإضافه البوت الى سيرفرك بريفكس البوت [-]**`)
+    .setDescription(`**[-] شكراً لك لإضافه البوت الى سيرفرك بريفكس البوت**`)
         guild.owner.send(embed)
   });
 
@@ -829,7 +829,7 @@ client.on('message', message => {
 
 client.on('guildMemberAdd', member => {
   
-  const channel = member.guild.channels.find(ch => ch.name === 'chat');
+  const channel = member.guild.channels.find(ch => ch.name === 'army');
  
   if (!channel) return;
 
@@ -842,7 +842,7 @@ client.on('guildMemberAdd', member => {
  
   if (!channel) return;
 
-  channel.send(`**Welcome To** __**The Army**__ :two_hearts: :coffee:, ${member}`);
+  channel.send(` **Welcome To Server **:two_hearts: :coffee:, ${member}`);
 });
 
 client.on('message', message => {
@@ -952,5 +952,79 @@ EpicEdiTeD[message.author.id].Xp+= 0.25;
 EpicEdiTeD[message.author.id].Money+= 0.25;
  
 });
+
+const reply = JSON.parse(fs.readFileSync('./replys.json' , 'utf8'));
+client.on('message', async message => {
+    let messageArray = message.content.split(" ");
+   if(message.content.startsWith(prefix + "setReply")) {
+             
+    let filter = m => m.author.id === message.author.id;
+    let thisMessage;
+    let thisFalse;
+
+    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('You don\'t have permission').then(msg => {
+       msg.delete(4500);
+       message.delete(4500);
+    });
+    
+    message.channel.send(':pencil: **| من فضلك اكتب الرساله الان... :pencil2: **').then(msg => {
+
+        message.channel.awaitMessages(filter, {
+          max: 1,
+          time: 90000,
+          errors: ['time']
+        })
+        .then(collected => {
+            collected.first().delete();
+            thisMessage = collected.first().content;
+            let boi;
+            msg.edit(':scroll: **| من فضلك اكتب الرد الان... :pencil2: **').then(msg => {
+      
+                message.channel.awaitMessages(filter, {
+                  max: 1,
+                  time: 90000,
+                  errors: ['time']
+                })
+                .then(collected => {
+                    collected.first().delete();
+                    boi = collected.first().content;
+                    msg.edit('✅ **| تم الاعداد بنجاح...  **').then(msg => {
+        
+                      message.channel.awaitMessages(filter, {
+                        max: 1,
+                        time: 90000,
+                        errors: ['time']
+                      })
+                      let embed = new Discord.RichEmbed()
+                      .setTitle('**Done The Autoreply Code Has Been Setup**')
+                      .addField('Message:', `${thisMessage}`)
+                      .addField('Reply:', `${boi}`)
+                      .setThumbnail(message.author.avatarURL)
+                      .setFooter(`${client.user.username}`)
+                     message.channel.sendEmbed(embed)
+    reply[message.guild.id] = {
+        msg: thisMessage,
+        reply: boi,
+        onoff: 'On'
+    }
+    fs.writeFile("./replys.json", JSON.stringify(reply), (err) => {
+    if (err) console.error(err)
+  })
+   } 
+            )
+        })
+    })
+})
+    })
+}})             
+client.on('message', async message => {
+            if(!reply[message.guild.id]) reply[message.guild.id] = {
+          onoff: 'Off'
+        }
+          if(reply[message.guild.id].onoff === 'Off') return;
+   if(message.content === reply[message.guild.id].msg) {
+       message.channel.send(reply[message.guild.id].reply)
+   }}
+)
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
