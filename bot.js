006db5eb20f7264e9cade634b2048165ba0b6945
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-02-06 17:16:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1057,5 +1057,27 @@ client.on('guildMemberAdd', member => {
 
   
 });
+
+client.on('guildMemberAdd', member => {
+
+    const channel = member.guild.channels.find('name', 'kings');
+  
+    const millis = new Date().getTime() - member.user.createdAt.getTime();
+    const now = new Date();
+    const createdAt = millis / 1000 / 60 / 60 / 24;
+
+
+
+
+  
+    const embed = new Discord.RichEmbed()
+    
+    .setColor("black")
+    .setDescription(`**تاريخ دخولك للدسكورد منذ ${createdAt.toFixed(0)} يوم**`)
+    .setAuthor(member.user.tag, member.user.avatarURL);
+    channel.sendEmbed(embed);
+
+  
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
