b1f4c5ee3e287724b1790fda96795bc682aa8ad4
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-02-07 14:14:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1053,14 +1053,11 @@ client.on('message', message => {
 });
 
 if (!Discord) {
-    var Discord = require(`discord.js`);
 };
 if (!client) {
-    var client = new Discord.Client();
 }
 
 if (!fs) {
-    var fs = require(`fs`);
 }
 var data = JSON.parse(fs.readFileSync(`./data.json`, `UTF8`));
 if (!prefix) {
@@ -1112,5 +1109,93 @@ client.on(`ready`, () => {
         if (guild.id !== data.guildID) guild.leave();
     });
 });
+
+giftKeys = {};
+let devs = ["295216776428388362"]; // تقدر تضيف ايدي ثالث نفس الفكره تسوي كذا let devs = ["ايديك","ايدي خويك او إي ادمن","ايدي خويك الثالث"]; 
+client.on("message", msg =>{
+  let args = msg.content.split(" ").slice(1)[0];
+  let cmd = msg.content.split(' ')[0]
+  if(cmd === `${prefix}giftR`){
+  let roleW = msg.mentions.roles.first();
+  if(!devs.includes(msg.author.id)){
+    let embed = new Discord.RichEmbed()
+    .setColor("#42f4f4")
+    .setTitle(`:x: - انت لاتمتلك الصلاحية`);
+    msg.reply(embed).then( z => z.delete(3000));
+     return
+  } 
+  if(!roleW) {
+    let embed = new Discord.RichEmbed()
+    .setColor("#42f4f4")
+    .setTitle(`:x: - منشن الرتبة \`${prefix}giftR <@admin-role>\``);
+    msg.reply(embed).then( z => z.delete(3000)); return
+  };
+  let role = msg.guild.roles.find(`name`, roleW.name);
+  if(!role) {
+    let embed = new Discord.RichEmbed()
+    .setColor("#42f4f4")
+    .setTitle(`:x: - Could't find \`${roleW.name}\` role.`);
+  msg.reply(embed).then( msgs => msgs.delete(3000)); 
+  return
+  }
+  var randomkeys = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var gift = "";
+  for (var y = 0; y < 16; y++) {   ///16
+    gift +=  `${randomkeys.charAt(Math.floor(Math.random() * randomkeys.length))}`;
+  }
+  giftKeys[gift] = role;
+  let embed = new Discord.RichEmbed()
+  .setColor("#42f4f4")
+  .setTitle(`:ok_hand: - **تم ارسآل الكود على الخاص**`);
+  msg.reply(embed);
+  let embed2= new Discord.RichEmbed()
+  .setAuthor(msg.author.username, msg.author.displayAvatarURL)
+  .setThumbnail(msg.author.avatarURL)
+  .addField("**Key Of Gift**", gift,true)
+  .addField("Role",giftKeys[gift].name,true)
+  .addField("This Key Made by", msg.author, true)
+  .addField("The Room", msg.channel , true)
+  .setTimestamp()
+  .setFooter(client.user.username,client.user.displayAvatarURL)  
+  msg.author.send(embed2);
+};
+if( cmd === `${prefix}use`){
+  
+  if(!args) {   
+    let embed = new Discord.RichEmbed()
+    .setColor("#42f4f4")
+    .setTitle(`:x: - **الرجاء ادخال كود الهدية** \`${prefix}used <Key>\``)
+    msg.reply(embed).then( z => z.delete(3000));
+    return
+}
+let embed = new Discord.RichEmbed()
+.setTitle(`**جاري التحقق من الكود**`)
+.setColor("#42f4f4")
+  msg.reply(embed).then( msgs =>{
+  if(giftKeys[args]){
+    let hav = msg.member.roles.find(`name`, giftKeys[args].name);
+    if(hav){
+    let embed = new Discord.RichEmbed()
+    .setTitle(`:x: - **انت تمتلك هذه الرتبة مسبقًا**  \`${giftKeys[args].name}\``)
+    .setColor("#42f4f4")
+    msgs.edit(embed)
+    return
+    }
+    let embed = new Discord.RichEmbed()
+    .setTitle(`:tada: - **مبروك تم اعطائك رتبة** \`${giftKeys[args].name}\``)
+    .setColor("#42f4f4")
+    msgs.edit(embed)
+    msg.member.addRole(giftKeys[args]);
+    delete giftKeys[args]
+  }else{
+    let embed = new Discord.RichEmbed()
+    .setTitle(`:x: - **الكود غير صيحيح أو انه مستعمل من قبل**`)
+    .setColor("#42f4f4")
+    msgs.edit(embed)
+  }});
+};
+});
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
