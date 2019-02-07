07501012b29bafb64609770445fa83bedf886764
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-02-07 14:37:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -749,7 +749,7 @@ let args = message.content.split(' ');
 client.on('guildCreate', guild => {
     var embed = new Discord.RichEmbed()
     .setColor(0x5500ff)
-    .setDescription(`**[-] شكراً لك لإضافه البوت الى سيرفرك بريفكس البوت**`)
+    .setDescription(`**[*] شكراً لك لإضافه البوت الى سيرفرك بريفكس البوت**`)
         guild.owner.send(embed)
   });
 
@@ -1029,62 +1029,6 @@ client.on('message', message => {
   .setColor("#ebf442");
   message.channel.send(embed);
 }
-});
-
-if (!Discord) {
-};
-if (!client) {
-}
-
-if (!fs) {
-}
-var data = JSON.parse(fs.readFileSync(`./data.json`, `UTF8`));
-if (!prefix) {
-} else {
-};
-client.on(`message`, msg => {
-    if (msg.author.bot) return;
-    if (!msg.content.startsWith(prefix)) return;
-    var args = msg.content.slice(prefix.length).split(` `);
-    var command = `${args[0]}`;
-    switch (command) {
-        case `vip`:
-            if (msg.author.id !== data.ownerID) return;
-            var cmd = args[1];
-            if (!cmd) return msg.reply(`what command you are looking for ?`);
-            switch(cmd) {
-                case `move`:
-                    var guild = args[2];
-                    if (!guild) return msg.reply(`I can't find this server \\:(`);
-                    if (isNaN(parseInt(guild))) return msg.reply(`I can't find this server \\:(`);
-                    if (guild.length !== msg.guild.id.length) return msg.reply(`I can't find this server \\:(`);
-                    msg.channel.send(`invite me to your new server: https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&guild_id=${guild}&permissions=2146958847`);
-                    data.guildID = guild;
-                break;
-                case `prefix`:
-                    var prfx = args[3];
-                    if (!prefix) return msg.reply(`prefix ?`);
-                    data.prefix = prfx.trim();
-                    msg.channel.send(`done , my prefix now is : ${prfx}`);
-                break;
-            }
-        break;
-    };
-    fs.writeFileSync(`./data.json`, JSON.stringify(data, (null, 4)));
-});
-client.on(`guildCreate`, guild => {
-    if (guild.id !== data.guildID) guild.leave();
-    setTimeout(function Sweetie(){
-        client.guilds.forEach(g => {
-            if (g.id !== data.guildID) guild.leave();
-        });
-    }, 5000);
-});
-
-client.on(`ready`, () => {
-    client.guilds.forEach(guild => {
-        if (guild.id !== data.guildID) guild.leave();
-    });
 });
 
 giftKeys = {};
