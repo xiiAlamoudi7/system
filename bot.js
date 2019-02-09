fc0a0a95a6771067e96549ecd3da9d8ac9181da0
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-02-09 18:34:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
-const = require('ms')
-const moment = require('moment');
 let prefix = "*"
 
 
