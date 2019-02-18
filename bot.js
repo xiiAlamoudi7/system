ef2f8d61f37f557a786d79b29d83e4b292e5f619
ZALABIA <41113180+iiUMeex@users.noreply.github.com>
2019-02-18 21:43:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
+const ownerid = ('295216776428388362')
+const package = ('package.json');
 let prefix = "p!"
 
 
