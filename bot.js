92567843908fa615b51f4e3c6028396935ab0f03
ZALABIA <41113180+iiUMeex@users.noreply.github.com>
2019-02-19 13:56:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2420,77 +2420,74 @@ client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name",".DARK"));
     });
 
-// By @L#7574
-var client = new Discord.Client();
-var fs = require('fs');
-try {
-    require("./localstorage.json");
-} catch (e) {
-    fs.writeFileSync('localstorage.json', '{\n\n}', function (err) {
-        if (err) throw err;
-    });
-}
+const moment = ('moment');
 
-var db = JSON.parse(fs.readFileSync("./localstorage.json", 'utf8'));
+    const money = require('discord-money');
+ 
+    // Define client for Discord
 
-client.on("message", async function(msg) {
-    if (msg.author.bot) return undefined;
-    if (msg.channel.type !== "text") return undefined;
-    else {
-
-        await fs.readFileSync("./localstorage.json", 'utf8');
-
-        if (!db[msg.author.id]) db[msg.author.id] = {
-            credits : 0,
-            messages : 0,
-        };
-        db[msg.author.id].messages+=1;
-        if (db[msg.author.id].messages == 5) {
-            db[msg.author.id].credits = db[msg.author.id].credits+Math.floor(Math.random() * 5) +1;
-            db[msg.author.id].messages = db[msg.author.id].messages = 0;
-        } // L#7574
-        fs.writeFile("./localstorage.json", JSON.stringify(db), function(err) {
-            if (err) throw err;
-        });
-        var args = msg.content.toLowerCase().split(" ");
-        var mention = msg.mentions.users.first() || client.users.get(msg.content.split(" ")[1]) || msg.author;
-
-        if (args[0].slice(prefix.length) === "blance" || args[0].slice(prefix.length) === "credit" || args[0].slice(prefix.length) === "credits") {
-            if (!db[mention.id]) db[mention.id] = {
-                credits : 0,
-                messages : 0,
-            };
-            if (mention.id == msg.author.id) {
-                word = "You";
-            } else {
-                word = `${mention}'s`;
-            }// L#7574
-            if (db[mention.id].credits <= 0) return msg.channel.send(`${word} don't have any credit!`);
-            if (db[mention.id].credits > 0 && db[mention.id].credits !== 0) return msg.channel.send(`${word} blance is ${db[mention.id].credits}`);
-        } else if (args[0].slice(prefix.length) === "transfer") {
-            if (!db[msg.author.id]) db[msg.author.id] = {
-                    credits : 0,
-                    messages: 0,
-                }
-            if (!db[mention.id]) {
-                db[mention.id] = {
-                    credits : 0,
-                    messages : 0,
-                }
-            }
-            if (isNaN(args[2]) || !args[2] || !mention || isNaN(args[2]) && !args[2] && !mention) return msg.channel.send(`?!`)
-            else {
-                if (db[msg.author.id].credits <= 0 && db[msg.author.id].credits <= args[2]) {
-                    return msg.channel.send(`You don't have enough credit to transfer!`)
-                }
-                else {
-                    db[msg.author.id].credits-=parseInt(args[2])
-                    db[mention.id].credits+=parseInt(args[2])
-                    return msg.channel.send(`${mention}, have ${db[mention.id].credits} credit now!`)
-                }
-            }
+ 
+    // This runs when a message is recieved...
+    client.on('message', message => {
+ 
+        // Prefix
+     
+        // Example: Fetching Balance
+        if (message.content.toUpperCase() === `${prefix}credits`) {
+ 
+            money.fetchBal(message.author.id).then((i) => { // money.fetchBal grabs the userID, finds it, and puts it into 'i'.
+                message.channel.send(`**Balance:** ${i.money}`);
+            })
+ 
+ 
         }
-    }
-});
+ 
+        // Example: Adding Money To A User
+        if (message.content.toUpperCase() === `${prefix}payyou`) {
+
+ //           money.updateBal(message.author.id, 1000000 /* Value *///) //.then((i) => { // money.updateBal grabs the (userID, value) value being how much you want to add, and puts it into 'i'.
+          /*       message.channel.send(`**You got $1000000!**\n**New Balance:** ${i.money}`);
+            })
+ 
+        }
+ 
+        // Example: Removing Money From A User
+        if (message.content.toUpperCase() === `${prefix}payfine`) {
+ 
+            money.updateBal(message.author.id, -500).then((i) => { // Since the 'value' is -500, it will 'add' -500, making the bal $500 lower.
+                message.channel.send(`**You paid your fine of $500!**\n**New Balance:** ${i.money}`);
+            })
+ 
+        }
+ 
+        // Example: Getting a daily reward
+        if (message.content.toUpperCase() === prefix + `daily`) {
+
+                if (money[message.author.username + message.guild.name] != moment().format('L')) {
+                    money[message.author.username + message.guild.name] = moment().format('L')
+                    money.updateBal(message.author.id, 500 ,200).then((i) => { // The daily ends of the day, so everyday they can get a daily bonus, if they missed it, they can't get it back again.
+                        message.channel.send({embed: {
+                            color: 3447003,
+                            description: 'Recieved your **$500** \`^daily`\. I think you should check \`^credits\`.',
+                            author: {
+                                name: `${message.author.username}#${message.author.discriminator}`,
+                                icon_url: message.author.avatarURL 
+                            }
+                        }});
+                    })
+                } else {
+                    message.channel.send({embed: {
+                        color: 3447003,
+                        description: 'You already recieved your \`^daily`\. Check later **' + moment().endOf('day').fromNow() + '**.', // When you got your daily already, this message will show up.
+                        author: {
+                            name: `${message.author.username}#${message.author.discriminator}`,
+                            icon_url: message.author.avatarURL 
+                        }
+                    }});
+                }
+            }
+ 
+ 
+    });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
