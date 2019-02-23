7fa0f0f8b81ba712bafe596dae9337452026384a
ZALABIA <41113180+iiUMeex@users.noreply.github.com>
2019-02-23 18:15:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1873,13 +1873,13 @@ if(message.content.startsWith(prefix + "daily")) {
 let cont = message.content.slice(prefix.length).split(" ");
 let args = cont.slice(2);
 let sender = message.author
-if(message.content.startsWith(prefix + '*credits')) {
-if (!args[0]) {message.channel.send(`**Usage: ${prefix}*credits @someone amount**`); 
+if(message.content.startsWith(prefix + 'trans')) {
+if (!args[0]) {message.channel.send(`**Usage: ${prefix}trans @someone amount**`); 
          return;
            }
         // We should also make sure that args[0] is a number
         if (isNaN(args[0])) {
-            message.channel.send(`**Usage: ${prefix}*credits @someone number**`);
+            message.channel.send(`**Usage: ${prefix}trans @someone number**`);
             return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
              }
              if(profile[message.author.id].credits < args[0]) return message.channel.send("**Your Credits is not enough  that**")
@@ -1887,8 +1887,8 @@ if(args[0].startsWith("-")) return  message.channel.send('**!! I Cant Do it**');
 				 let defineduser = '';
             let firstMentioned = message.mentions.users.first();
             defineduser = (firstMentioned)
-            if (!defineduser) return message.channel.send(`**Usage: ${prefix}*credits @someone number**`);
-            if(defineduser.id === message.author.id) return message.channel.send("***بجد والله ?!***")
+            if (!defineduser) return message.channel.send(`**Usage: ${prefix}trans @someone number**`);
+            if(defineduser.id === message.author.id) return message.channel.send("***Transfering to your self hah ?!***")
             var mentionned = message.mentions.users.first();
 if (!profile[sender.id]) profile[sender.id] = {}
 if (!profile[sender.id].credits) profile[sender.id].credits = 310;
@@ -1918,6 +1918,7 @@ mentionned.send(`:credit_card: | Transfer Receipt \`\`\`\`You have received ${ar
                message.channel.sendEmbed(embed)
         })
         })
+}
 });
 	
 // THIS  MUST  BE  THIS  WAY
